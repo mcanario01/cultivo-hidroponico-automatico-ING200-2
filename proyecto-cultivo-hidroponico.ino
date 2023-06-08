cc7f3be9fb54347e5f25358229f180168440061d
Martín Tomás Canario Dauros <71862023+mcanario01@users.noreply.github.com>
2023-06-08 12:09:51 -0400

    Adición de distintas pantallas en las interfaz
    Se añade el principio de cambio de pantallas en la interfaz del LCD

--- proyecto-cultivo-hidroponico.ino
+++ proyecto-cultivo-hidroponico.ino
@@ -5,6 +5,8 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+#define CANTIDAD_DE_PANTALLAS 4
+
 #define DELAY_PANTALLA 1000
 #define LIMITE_INFERIOR_SENSOR_LUZ 30
 #define LIMITE_SUPERIOR_SENSOR_LUZ 600
@@ -15,6 +17,7 @@
 #define DIRECCION_LCD 0x27
 
 //PINES BOTON Y LED
+#define BOTON2 8
 #define BOTON1 4
 #define LED1 6
 
@@ -38,6 +41,7 @@
 DHT sensor_tyh(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C pantalla(DIRECCION_LCD, 16, 2);
 ezButton boton(BOTON1);
+ezButton boton_interfaz(BOTON2);
 
 //CARACTERES PERSONALIZADOS PANTALLA
 
@@ -122,6 +126,8 @@ bool estadoLed = 0;
 
 unsigned long millis_pasados_pantalla = 0;
 
+int pantalla_interfaz_actual = 1;
+
 void setup() {
 
   Serial.begin(9600);
@@ -138,6 +144,7 @@ void setup() {
   pinMode(LED1, OUTPUT);   //declara pin de LEDs
   pinMode(BOCINA, OUTPUT); //declara pin de BUZZER
   boton.setDebounceTime(50); //declara tiempo de refresco de boton
+    boton_interfaz.setDebounceTime(50); //declara el tiempo de refresco de boton_interfaz
 
   //INICIALIZACIÓN DE CARACTERES PERSONALIZADOS
   pantalla.createChar(0, icono_alerta);
@@ -181,59 +188,83 @@ void loop() {
   digitalWrite(LED1, estadoLed); //escribe el estado del LED
 
   digitalWrite(BOMBA_AGUA, estadoLed); //escribe el estado de la bomba de agua
+    
+    if(boton_interfaz.isPressed())
+    {
+        pantalla_interfaz_actual++;
+        if(pantalla_interfaz_actual >= CANTIDAD_DE_PANTALLAS)
+        {
+            pantalla_interfaz_actual = 1;
+        }
+    }
 
   //ESCRUTURA EN PANTALLA ---------------------------------
   //pantalla.clear();
 
-  pantalla.setCursor(15,0);     //escritura de icono de LEDs
-  if(estadoLed)
-  {
-    pantalla.print((char)3);    //escritura de icono_luz_encendida
+    switch(pantalla_interfaz_actual)
+    {
+        
+        //Pantalla 1 - Vista general de datos
+        case 1:
+        {
+            pantalla.setCursor(15,0);     //escritura de icono de LEDs
+            if(estadoLed)
+                {
+                pantalla.print((char)3);    //escritura de icono_luz_encendida
+                }
+            else
+                {
+                pantalla.print((char)4);    //escritura de icono_luz_apagada
+                }
+            pantalla.setCursor(15,1);
+            pantalla.print((char)0);      //icono_alerta
+            //Actualización de pantalla con diferencial del tiempo. (Para leer mejor los valores)
+            unsigned long millis_actuales_pantalla = millis();
+            if(millis_actuales_pantalla - millis_pasados_pantalla >= DELAY_PANTALLA )
+                {
+                millis_pasados_pantalla = millis_actuales_pantalla;
+                pantalla.setCursor(0,0);
+                pantalla.print((char)1);      //escritura de icono_temperatura
+                pantalla.setCursor(1,0);
+                pantalla.print((int)sensor_tyh.readTemperature()); //escritura de temperatura
+                pantalla.print("  ");
+                
+    
+                pantalla.setCursor(0,1);
+                pantalla.print((char)2);      //escritura de icono_humedad
+                pantalla.setCursor(1,1);
+                pantalla.print((int)sensor_tyh.readHumidity()); //escritura de humedad
+                pantalla.print("  ");
+
+                pantalla.setCursor(5,0);
+                pantalla.print((char)5);      //escritura de icono_nivel_agua
+                pantalla.setCursor(6,0);
+                pantalla.print(map(analogRead(NIVEL_AGUA), LIMITE_INFERIOR_SENSOR_NIVEL_AGUA, LIMITE_SUPERIOR_SENSOR_NIVEL_AGUA, 0, 99)); //escritura de nivel de agua
+                pantalla.print("  ");
+
+                pantalla.setCursor(5,1);
+                pantalla.print((char)6);      //escritura de icono_nivel_luz
+                pantalla.setCursor(6,1);
+                pantalla.print(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99)); //nivel de luz
+                pantalla.print("  ");
+
+
+                //Impresión por monitor serial de valores de pruebas
+                Serial.print(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99));
+                Serial.print(" - ");
+                Serial.println(-(1/sqrt(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99)))+99);
   }
-  else
-  {
-    pantalla.print((char)4);    //escritura de icono_luz_apagada
-  }
-
-  pantalla.setCursor(15,1);
-  pantalla.print((char)0);      //icono_alerta
-
-  //Actualización de pantalla con diferencial del tiempo. (Para leer mejor los valores)
-  unsigned long millis_actuales_pantalla = millis();
-  if(millis_actuales_pantalla - millis_pasados_pantalla >= DELAY_PANTALLA )
-  {
-    millis_pasados_pantalla = millis_actuales_pantalla;
-
-    pantalla.setCursor(0,0);
-    pantalla.print((char)1);      //escritura de icono_temperatura
-    pantalla.setCursor(1,0);
-    pantalla.print((int)sensor_tyh.readTemperature()); //escritura de temperatura
-    pantalla.print("  ");
-
-    pantalla.setCursor(0,1);
-    pantalla.print((char)2);      //escritura de icono_humedad
-    pantalla.setCursor(1,1);
-    pantalla.print((int)sensor_tyh.readHumidity()); //escritura de humedad
-    pantalla.print("  ");
-
-    pantalla.setCursor(5,0);
-    pantalla.print((char)5);      //escritura de icono_nivel_agua
-    pantalla.setCursor(6,0);
-    pantalla.print(map(analogRead(NIVEL_AGUA), LIMITE_INFERIOR_SENSOR_NIVEL_AGUA, LIMITE_SUPERIOR_SENSOR_NIVEL_AGUA, 0, 99)); //escritura de nivel de agua
-    pantalla.print("  ");
-
-    pantalla.setCursor(5,1);
-    pantalla.print((char)6);      //escritura de icono_nivel_luz
-    pantalla.setCursor(6,1);
-    pantalla.print(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99)); //nivel de luz
-    pantalla.print("  ");
-
-
-    //Impresión por monitor serial de valores de pruebas
-    Serial.print(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99));
-    Serial.print(" - ");
-    Serial.println(-(1/sqrt(map(analogRead(NIVEL_LUZ), LIMITE_INFERIOR_SENSOR_LUZ, LIMITE_SUPERIOR_SENSOR_LUZ, 0, 99)))+99);
-  }
+        }
+        
+        //Pantalla 2 - Ajuste de LEDS
+        case 2:
+        {
+            
+        }
+    }
+    
+    
+  
   
 
 }
